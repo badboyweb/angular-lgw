5bf24fa1403a913081d97b1ee872042929f2d4e8
badboyweb <nashwq@163.com>
2018-03-19 10:58:52 +0800

    修改文件路径

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module("app").controller('mainCtrl',['$scope','$http',function ($scope,$http) {
-    $http.get('/data/positionList.json').success(
+    $http.get('data/positionList.json').success(
         function (resp) {
            //console.log(resp);
             $scope.list=resp
