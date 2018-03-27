a020434da51469999367b8768f339adc2d4799f7
badboyweb <nashwq@163.com>
2018-03-27 16:53:33 +0800

    delete file

--- src/script/controller/favoriteCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('app').controller('favoriteCtrl',['$scope','$http',function ($scope,$http) {
-    $http.get('data/myFavorite.json').success(function (resp) {
-        $scope.list = resp;
-    })
-
-}]);
--- src/script/controller/mainCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module("app").controller('mainCtrl',['$scope','$http',function ($scope,$http) {
-    $http.get('data/positionList.json').success(
-        function (resp) {
-           //console.log(resp);
-            $scope.list=resp
-        }
-    );
-}]);
